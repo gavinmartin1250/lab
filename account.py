8a711c5f26834b2dfe09d238da51d826d34b649c
gavinmartin1250 <102701856+gavinmartin1250@users.noreply.github.com>
2023-04-20 23:53:38 -0500

    Doc Changes

--- account.py
+++ account.py
@@ -1,24 +1,25 @@
 class Account:
-    def __init__(self,name):
+    def __init__(self,name: str) -> None:
         self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self,amount):
+    def deposit(self,amount) -> bool:
         if amount > 0:
             self.__account_balance += amount
             return True
         else:
             return False
 
-    def withdraw(self, amount):
+    def withdraw(self, amount) -> bool:
         if amount > 0 and self.__account_balance > amount:
             self.__account_balance = self.__account_balance - amount
             return True
         else:
             return False
 
-    def get_balance(self):
+    def get_balance(self) -> float:
         return self.__account_balance
 
-    def get_name(self):
+    def get_name(self) -> str:
         return self.__account_name
+
